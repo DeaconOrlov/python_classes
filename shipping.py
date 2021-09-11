c71e3c22bacba65320332611c7e60607179e25f2
DeaconOrlov <deaconorlov@gmail.com>
2021-09-10 21:30:40 -0400

    refactor _generate_serial as classmethod

--- shipping.py
+++ shipping.py
@@ -2,14 +2,14 @@ class ShippingContainer:
 
     next_serial = 1337
 
-    @staticmethod
-    def _generate_serial():
+    @classmethod
+    def _generate_serial(cls):
 
-        result = ShippingContainer.next_serial
-        ShippingContainer.next_serial += 1
+        result = cls.next_serial
+        cls.next_serial += 1
         return result
 
     def __init__(self, owner_code, contents):
        self.owner_code = owner_code
        self.contents = contents 
-       self.serial = self._generate_serial()
+       self.serial = ShippingContainer._generate_serial()
