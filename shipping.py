7fc316ae54470f080bb554469b556b64c4d8e0c5
DeaconOrlov <deaconorlov@gmail.com>
2021-09-12 10:37:48 -0400

    add ISO serial generator module

--- shipping.py
+++ shipping.py
@@ -9,6 +9,14 @@ class ShippingContainer:
         cls.next_serial += 1
         return result
 
+    @classmethod
+    def create_empty(cls, owner_code):
+        return cls(owner_code, contents=[])
+
+    @classmethod
+    def create_with_items(cls, owner_code, items):
+        return cls(owner_code, contents=list(items))
+
     def __init__(self, owner_code, contents):
        self.owner_code = owner_code
        self.contents = contents 
